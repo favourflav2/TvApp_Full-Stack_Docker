6d9099c35371a5c0557b3fdbe883c6589df9b7f6
favourflav <chukwagon1@gmail.com>
2023-07-29 10:38:45 -0700

    still trying to fix rate limiter

--- server/app.js
+++ server/app.js
@@ -13,6 +13,10 @@ const app = express()
 dotenv.config()
 app.use(morgan("dev"))
 app.use(helmet())
+if(process.env.NODE_ENV == 'prod'){
+  app.set('trust proxy', 1)
+  app.get('/ip', (request, response) => response.send(request.ip))
+}
 // cors options
 const whiteList = [`${process.env.PRODUCTION_URL}`,`${process.env.LOCALHOST_URL}`,'http://localhost:5001/']
 var corsOptions = {
