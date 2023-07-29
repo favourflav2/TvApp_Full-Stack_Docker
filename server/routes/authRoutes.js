7968e468d18c102ee632ecd2a802ee625c4338e4
favourflav <chukwagon1@gmail.com>
2023-07-29 10:27:30 -0700

    still trying to fix rate limtier

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,10 +4,9 @@ import { authMiddleware } from "../middleware/authMiddleware.js";
 import rateLimit from 'express-rate-limit'
 
 const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
+	windowMs: 1 * 60 * 1000, // 15 minutes
 	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+	message: "Too many requests from this IP, please try again later."
 })
 
 const router = Router()
@@ -16,7 +15,7 @@ router.post("/signup",sign_Up)
 router.post("/login",log_In)
 router.post("/google",google_Sign_In)
 
-router.post("/likeTv",authMiddleware,limiter,like_Tv)
-router.get("/getLikedTv",authMiddleware,limiter,get_Liked_Tv)
+router.post("/likeTv",limiter,authMiddleware,like_Tv)
+router.get("/getLikedTv",limiter,authMiddleware,get_Liked_Tv)
 
 export default router
